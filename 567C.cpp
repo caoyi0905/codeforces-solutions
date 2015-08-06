3ec71c67ac0f26d7d059b10fc5b8cfc2d1cd5d61
caoyi <caoyi0905@163.com>
2015-08-06 16:40:18 +0800

    2015年8月6日
    2015年8月6日

--- 567C.cpp
+++ 567C.cpp
@@ -9,40 +9,19 @@ inline void read(__ll &m)
     while(ch>='0'&&ch<='9'){x=x*10+ch-'0';ch=getchar();}
     m=x*f;
 }
-ll a[211111];
-int cnt[211111];
-ll ans=0;
-map<int,int> mp,mp2;
+ll ans;
+map<int,ll> mp,mp2;
 int main()
 {
-    int i,n;ll k;
+    int i,n,x;ll k;
     cin>>n>>k;
-    for(i=1;i<=n;i++) read(a[i]);
-    if(k==1){
-        sort(a+1,a+n+1);
-        for(i=1;i<=n;i++){
-            int st=lower_bound(a+i+1,a+n+1,a[i]*k)-a;
-            if(a[st]!=a[i]*k) continue;
-            int ed=lower_bound(a+i+1,a+n+1,a[i]*k+1)-a;
-            ll l1=ed-st;
-            ans+=l1*(l1-1)/2;
-        }
-        cout<<ans<<endl;
-        return 0;
-    }
-    for(i=1;i<=n;i++) mp2[a[i]]++;
     for(i=1;i<=n;i++){
-        if(a[i]%k!=0){
-            mp[a[i]]++;
-            continue;
+        read(x);
+        if(x%k==0){
+            ans+=mp[x/k];
+            mp[x]+=mp2[x/k];
         }
-        if(a[i]*k>1000000000) continue;
-        mp2[a[i]]--;
-        int t=mp2[a[i]*k];
-        if(t!=0){
-            ans+=(ll)mp[a[i]/k]*t;
-        }
-        mp[a[i]]++;
+        mp2[x]++;
     }
     cout<<ans<<endl;
 }
